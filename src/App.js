559859e66f0f8f5d41e4bc1068259daaf0a02bc4
varshu2006 <varshinisrivarshu@gmail.com>
2025-09-22 14:12:54 +0530

    forth commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 style={{color:'blueviolet'}}>Hi, Iam varshu. This is my First Deployment..!!</h1>
-      <h2>Iam hiring 13 LPA and Iam hiring 3 months of Web development internship in Bangalore.</h2>
+      <h2>Iam hiring 13 LPA and Iam hiring 6 months of Web development internship in Bangalore(3months) and Chennai(3months).</h2>
     </div>
   );
 }
