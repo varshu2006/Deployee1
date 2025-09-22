8ce2c20fab7c043ee2f7cecc3721976ff90e0a56
varshu2006 <varshinisrivarshu@gmail.com>
2025-09-22 14:09:29 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 style={{color:'blueviolet'}}>Hi, Iam varshu. This is my First Deployment..!!</h1>
+      <h2>Iam hiring 13 LPA and Iam hiring 3 months of Web development internship in Bangalore.</h2>
     </div>
   );
 }
